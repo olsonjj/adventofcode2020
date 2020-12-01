054b0d7308429f8c234aec4ab023c194bde99a53
John Olson <jolson@CHQ-DEVLAP1236.local>
2020-12-01 08:03:25 -0800

    day 1 advent of code

---
+++ day1/index.ts
@@ -0,0 +1,23 @@
+import { input } from './input';
+let a = 0;
+for (let a = 0; a < input.length; a++) {
+    for (let b = a+1; b < input.length; b++) {
+        if (input[a] + input[b] === 2020) {
+            console.log('part 1', input[a] * input[b])
+        }
+    }
+}
+
+
+for (let a = 0; a < input.length; a++) {
+    for (let b = a+1; b < input.length; b++) {
+        for (let c = b+1; c < input.length; c++) {
+            const part2 = input[a] + input[b] + input[c];
+            if (part2 === 2020) {
+                console.log('part 2', input[a] * input[b] * input[c]);
+            }
+        }
+    }
+}
+
+console.log('done')
