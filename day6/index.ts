88e893db18c95d60155bfebe06cd5ff8fb6e2685
John Olson <jolson@CHQ-DEVLAP1236.local>
2020-12-06 11:13:33 -0800

    day 6

---
+++ day6/index.ts
@@ -0,0 +1,39 @@
+import { customsData } from './input';
+
+const forms = customsData.split('\n');
+
+const groupList = forms.reduce(
+  (acc, curr, index) => {
+    if (curr.trim().length === 0) {
+      acc.groups.push(acc.temp);
+      acc.temp = [];
+    } else {
+      acc.temp = acc.temp.concat(curr);
+      if (index === forms.length - 1) {
+        acc.groups.push(acc.temp);
+      }
+    }
+    return acc;
+  },
+  { groups: [], temp: [] }
+);
+
+const partACount = groupList.groups.reduce((count: number, group: string[]) => {
+  return count + new Set(group.join('')).size;
+}, 0);
+
+const partBCount = groupList.groups.reduce((count, group: string[]) => {
+  if (group.length === 1) {
+    return count + new Set(group.join('')).size;
+  }
+  const ct = group.reduce((acc, curr) => {
+    const intersection = acc
+      .split('')
+      .filter((char) => curr.split('').includes(char));
+    return intersection.join('');
+  });
+  return count + ct.length;
+}, 0);
+
+console.log('Part A group count', partACount);
+console.log('Part B group count', partBCount);
